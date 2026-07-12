d958d6df85c1e99db74335179b7833724e9482a8
pedro <pedro@PEDROIGHORNOTEB>
2026-07-11 21:38:14 -0300

    criação da camada shared do projeto

---
+++ src/PaymentsAPI.Shared/Helpers/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace PaymentsAPI.Shared.Helpers
+{
+    public static class Utils
+    {
+        public static string Description(this Enum value)
+        {
+            {
+                DescriptionAttribute[]? array =
+                    (DescriptionAttribute[]?)value.GetType()
+                        .GetField(value.ToString())
+                        ?.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                return (array != null && array.Length != 0)
+                    ? array[0].Description
+                    : value.ToString();
+            }
+        }
+    }
+}
---
+++ src/PaymentsAPI.Shared/Logging/LogMessages.cs
@@ -0,0 +1,29 @@
+﻿using PaymentsAPI.Domain.Entitities;
+
+namespace PaymentsAPI.Shared.Logging
+{
+    public static class LogMessages
+    {
+        #region Payment Validation
+
+        public static string InvalidPaymentInputs() => "Invalid payment data.";
+
+        #endregion
+
+        #region Payment CRUD
+
+        public static string AddingPaymentError(Exception ex) =>
+            $"Error processing payment. Details: {ex.Message}";
+
+        public static string AddingPaymentSuccess(PaymentEntity paymentEntity) =>
+            $"Payment id:{paymentEntity.Id} processed successfully.";
+
+        public static string GetByPaymentIdError(Exception ex) =>
+            $"Error retrieving Payment by id. Details: {ex.Message}";
+
+        public static string GetByPaymentIdSuccess(PaymentEntity paymentEntity) =>
+            $"Payment id:{paymentEntity.Id} retrieved successfully.";
+
+        #endregion
+    }
+}
